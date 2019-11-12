8fd12e60ef0d8fa0fd8640aeeeb45a7e70fb73d4
Emi1699 <emisv_16@yahoo.ro>
2019-11-12 17:44:13 +0000

    created Rotor, Reflector and Offsets types; created encode function to be used later

--- Assignment2.hs
+++ Assignment2.hs
@@ -1,7 +1,21 @@
 
 
-module Assignment2
-	
+module Assignment2 where
+	import AssignmentHelp
+
+	type Rotor = Cipher
+	type Reflector = [(Char, Char)]
+	type Offsets = (Int, Int, Int)
+
+	data Enigma = SimpleEnigma Rotor Rotor Rotor Reflector Offsets
+
+	enigmaEncode :: Char -> Enigma -> Char
+	enigmaEncode = 
+		| 
+		| otherwise = 
 
 
-
+	encode :: Cipher -> Int -> Char -> Char
+	encode cipher offset character
+		| (alphaPos character - offset) `mod` 26 == (alphaPos (currentAlphabetChr cipher)) `mod` 26 = head cipher 
+		| otherwise = encode (tail cipher) offset character
