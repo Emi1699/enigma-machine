774307c0e9a462c34812ca585beec7d1f00a6c04
Emi1699 <emisv_16@yahoo.ro>
2019-11-17 18:14:08 +0000

    created advanceRotors() method, used after encoding each character in a message

--- Assignment2.hs
+++ Assignment2.hs
@@ -9,8 +9,10 @@ module Assignment2 where
 
 	data Enigma = SimpleEnigma Rotor Rotor Rotor Reflector Offsets
 
+	my_enigma = SimpleEnigma rotor1 rotor2 rotor3 reflectorB (0, 0, 0)
+
 	enigmaEncode :: Char -> Enigma -> Char
-	enigmaEncode = 
+	enigmaEncode c (SimpleEnigma lr mr rr ref (ol, om, or))
 		| 
 		| otherwise = 
 
@@ -19,3 +21,13 @@ module Assignment2 where
 	encode cipher offset character
 		| (alphaPos character - offset) `mod` 26 == (alphaPos (currentAlphabetChr cipher)) `mod` 26 = head cipher 
 		| otherwise = encode (tail cipher) offset character
+
+
+	advanceRotors :: Offsets -> Offsets
+	advanceRotors (ol, om, or) =
+		if or == 25 then
+			if om == 25 then
+				if ol == 25 then (0, 0, 0)
+				else (ol + 1, 0, 0)
+			else (ol, om + 1, 0)
+		else (ol, om, or + 1)
