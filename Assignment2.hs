23da095f433120b343eca7495f372b347f658e6d
Emi1699 <emisv_16@yahoo.ro>
2019-11-17 19:07:53 +0000

    added encode() -> method which lets us encode 1 single character based on 1 rotor's configuration and its offset

--- Assignment2.hs
+++ Assignment2.hs
@@ -3,6 +3,8 @@
 module Assignment2 where
 	import AssignmentHelp
 
+	import Data.Char
+
 	type Rotor = Cipher
 	type Reflector = [(Char, Char)]
 	type Offsets = (Int, Int, Int)
@@ -11,16 +13,18 @@ module Assignment2 where
 
 	my_enigma = SimpleEnigma rotor1 rotor2 rotor3 reflectorB (0, 0, 0)
 
-	enigmaEncode :: Char -> Enigma -> Char
-	enigmaEncode c (SimpleEnigma lr mr rr ref (ol, om, or))
-		| 
-		| otherwise = 
+	-- enigmaEncode :: Char -> Enigma -> Char
+	-- enigmaEncode c (SimpleEnigma lr mr rr ref (ol, om, or))
+	-- 	| 
+	-- 	| otherwise = 
 
 
-	encode :: Cipher -> Int -> Char -> Char
-	encode cipher offset character
-		| (alphaPos character - offset) `mod` 26 == (alphaPos (currentAlphabetChr cipher)) `mod` 26 = head cipher 
-		| otherwise = encode (tail cipher) offset character
+	-- encodes 1 character based on one rotor's cipher and its corresponding offset
+	-- this method captures the functionality of one single rotor
+	encode :: Char -> Rotor -> Int -> Char
+	encode character rotor offset
+		| (alphaPos character + offset) `mod` 26 == (alphaPos (currentAlphabetChr rotor)) `mod` 26 = chr ((((alphaPos (head rotor)) - offset) `mod` 26) + 65)
+		| otherwise = encode character (tail rotor) offset
 
 
 	advanceRotors :: Offsets -> Offsets
@@ -31,3 +35,6 @@ module Assignment2 where
 				else (ol + 1, 0, 0)
 			else (ol, om + 1, 0)
 		else (ol, om, or + 1)
+
+	currentAlphabetChr :: String -> Char
+	currentAlphabetChr cipher = chr (91 - length cipher)
